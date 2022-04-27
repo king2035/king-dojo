9b5fda8e7854c466c084b5268d3abcbdcd6292a1
墨香阁主 <ghost@beijinghejubianyanjiusuo749juguangyanGzu.local>
2022-04-27 11:01:50 +0800

    重新初始化项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    // HelloWorld
+  },
+  setup () {
+    const counter = ref(0)
+    console.log(this)
+    console.log('1232', counter.value)
+    return {
+      counter
+    }
+  }
+})
+</script>
