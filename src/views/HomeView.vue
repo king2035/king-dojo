68b704fc5466ffe587fb908daa295912f41428c8
墨香阁主 <ghost@beijinghejubianyanjiusuo749juguangyanGzu.local>
2022-04-29 18:05:28 +0800

    fix some bug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
+  <div class="home-1">
+    <h1>十里平湖霜满天</h1>
+  </div>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
     <p>{{ counter }}</p>
+    <ul>
+      <li>{{ users }}</li>
+      <li v-for="item in test" :key="item">{{ item }}</li>
+    </ul>
+    <h1>{{ name }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, Ref, reactive } from 'vue'
 // import HelloWorld from '@/components/HelloWorld.vue'
 
 export default defineComponent({
@@ -15,13 +23,44 @@ export default defineComponent({
   components: {
     // HelloWorld
   },
-  setup () {
-    const counter = ref(0)
-    console.log(this)
-    console.log('1232', counter.value)
+  data () {
     return {
-      counter
+      name: 'Super Man'
     }
+  },
+  setup () {
+    interface Book {
+      title: string,
+      year?: number
+    }
+    const counter = ref(0)
+    const users = ref('')
+    const test: Ref<Array<string>> = ref([])
+    const book: Book = reactive({ title: 'Vue3 指引' })
+
+    const getUsers = () => {
+      users.value = 'hello world!!!'
+      test.value = [
+        'Tom',
+        'jerry'
+      ]
+    }
+
+    onMounted(getUsers)
+
+    return {
+      counter,
+      users,
+      test,
+      book,
+      getUsers
+    }
+  },
+
+  mounted () {
+    console.log('users => ', this.users)
+    console.log('test :>> ', this.test)
+    console.log('book :>> ', this.book)
   }
 })
 </script>
